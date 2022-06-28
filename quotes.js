a6cc180a9d3de6e47b787e494edbfbb0e2f6fadc
DVNJNKNS <devonmjenkins3@gmail.com>
2022-06-28 02:21:04 -0400

    add more quotes

--- quotes.js
+++ quotes.js
@@ -1,13 +1,14 @@
 export const quotes = [
-  "hello governover", "grumpy scmonky",
-  "i lost my foot in the war", "I'm a saaaaaaaad panda",
-  "say hello to my little friend", "give em the goof balls",
-  "honey I shrunk the kids", "whos line is it anyways",
-  "number 7, the twerking chicken", "welcome to hell",
-  "here's johnny", "its a me mario", "oh hi mark",
-  "there's too many cooks in the kitchen", "damn we're in a tight spot",
-  "Hello. My name is Inigo Montoya. You killed my father. Prepare to die",
-  'I have a dream that one day this nation will rise up and live out the true meaning of its creed: "We hold these truths to be self-evident, that all men are created equal. I have a dream that one day on the red hills of Georgia, the sons of former slaves and the sons of former slave owners will be able to sit down together at the table of brotherhood. I have a dream that one day even the state of Mississippi, a state sweltering with the heat of injustice, sweltering with the heat of oppression, will be transformed into an oasis of freedom and justice. I have a dream that my four little children will one day live in a nation where they will not be judged by the color of their skin but by the content of their characr I have a dream today!',
-  'Everett is coming up with really long quotes'
+	"hello governover", "grumpy scmonky",
+	"i lost my foot in the war", "I'm a saaaaaaaad panda",
+	"say hello to my little friend", "give em the goof balls",
+	"honey I shrunk the kids", "whos line is it anyways",
+	"number 7, the twerking chicken", "welcome to hell",
+	"here's johnny", "its a me mario", "oh hi mark",
+	"there's too many cooks in the kitchen", "damn we're in a tight spot",
+	"Hello. My name is Inigo Montoya. You killed my father. Prepare to die",
+	'I have a dream that one day this nation will rise up and live out the true meaning of its creed: "We hold these truths to be self-evident, that all men are created equal. I have a dream that one day on the red hills of Georgia, the sons of former slaves and the sons of former slave owners will be able to sit down together at the table of brotherhood. I have a dream that one day even the state of Mississippi, a state sweltering with the heat of injustice, sweltering with the heat of oppression, will be transformed into an oasis of freedom and justice. I have a dream that my four little children will one day live in a nation where they will not be judged by the color of their skin but by the content of their characr I have a dream today!',
+	'Everett is coming up with really long quotes',
+	'Attitude is a little thing that makes a BIG difference.” “Success is not final, failure is not fatal, it is the courage to continue that counts.” “If youre going through hell, keep going.” "Everyone has his day, and some days last longer than others.',
 ]
 //export default [quotes] 
